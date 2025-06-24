6ab9383a7d1a193ea7f56dada3c1257b7868749f
Rajnish Mandavariya <147729116+rajnishmanda1@users.noreply.github.com>
2025-06-24 18:54:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,16 @@
 import streamlit as st
+from googletrans import Translator
 
-st.title('🎈 App Name')
+def translate_en_to_hi(text):
+    translator = Translator()
+    translated = translator.translate(text, src='en', dest='hi')
+    return translated.text
 
-st.write('Hello world!')
+st.title("Translate from English to Hindi")
+
+user_input = st.text_input("Enter some text:")
+
+if user_input:
+    st.write("In English: ", user_input)
+    hindi_translation = translate_en_to_hi(user_input)
+    st.write("In Hindi: ", hindi_translation)
